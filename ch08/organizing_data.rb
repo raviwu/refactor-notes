72f9f50c1372a25e2489d11a1d0932eb1a3e0be0
Ravi Wu <raviwu@gmail.com>
2017-03-20 14:52:59 +0800

    add examples for Replace Type Code with State/Strategy

--- ch08/organizing_data.rb
+++ ch08/organizing_data.rb
@@ -20,3 +20,179 @@
 # - 如果 typecode 不影響 class 行為：Replace Typecode with Class
 # - 如果 typecode 影響 class 行為：Replace Typecode with Subclasses 若
 #   不能處理，可再利用 Replace Typecode with State/Strategy
+
+# Replace Type Code with Subclasses
+# =========================================
+
+class Employee
+  ENGINEER = 0
+  SALESMAN = 1
+  MANAGER = 3
+
+  def create(type)
+    case type
+    when ENGINEER then Engineer.new
+    when SALESMAN then Salesman.new
+    when MANAGER then Manager.new
+    else raise 'Invalid Type Code'
+    end
+  end
+end
+
+class Engineer < Employee
+  def type
+    Employee::ENGINEER
+  end
+end
+
+class Salesman < Employee
+  def type
+    Employee::SALESMAN
+  end
+end
+
+class Manager < Employee
+  def type
+    Employee::MANAGER
+  end
+end
+
+# Replace Type Code with State/Strategy
+# =========================================
+# 新增一個 class 把狀態封存成單一的 switch 準備好往下繼續拆 Replace Conditional with Polymorphism
+
+class Employee
+  attr_reader :type
+
+  ENGINEER = 0
+  SALESMAN = 1
+  MANAGER = 3
+
+  def initialize(type)
+    @type = type
+  end
+
+  def pay_amount
+    case type
+    when ENGINEER then monthly_salary
+    when SALESMAN then monthly_salary + commission
+    when MANAGER then monthly_salary + bonus
+    else raise 'Invalid Employee'
+    end
+  end
+end
+
+# -----------------------------------------
+
+class Employee
+  attr_reader :employee_type
+
+  def initialize(type)
+    @employee_type = EmployeeType.new(type)
+  end
+
+  def pay_amount
+    case employee_type.type
+    when EmployeeType::ENGINEER then monthly_salary
+    when EmployeeType::SALESMAN then monthly_salary + commission
+    when EmployeeType::MANAGER then monthly_salary + bonus
+    else raise 'Invalid Employee'
+    end
+  end
+
+  #...
+end
+
+class EmployeeType
+  ENGINEER = 0
+  SALESMAN = 1
+  MANAGER = 3
+
+  def initialize(type)
+    case type
+    when ENGINEER then Engineer.new
+    when SALESMAN then Salesman.new
+    when MANAGER then Manager.new
+    else raise 'Invalid Employee'
+    end
+  end
+end
+
+class Engineer < EmployeeType
+  def type
+    Employee::ENGINEER
+  end
+end
+
+class Salesman < EmployeeType
+  def type
+    Employee::SALESMAN
+  end
+end
+
+class Manager < EmployeeType
+  def type
+    Employee::MANAGER
+  end
+end
+
+# -----------------------------------------
+
+class Employee
+  attr_reader :employee_type
+
+  def initialize(type)
+    @employee_type = EmployeeType.new(type)
+  end
+
+  def pay_amount
+    employee_type.pay_amount
+  end
+
+  #...
+end
+
+class EmployeeType
+  ENGINEER = 0
+  SALESMAN = 1
+  MANAGER = 3
+
+  def initialize(type)
+    case type
+    when ENGINEER then Engineer.new
+    when SALESMAN then Salesman.new
+    when MANAGER then Manager.new
+    else raise 'Invalid Employee'
+    end
+  end
+end
+
+class Engineer < EmployeeType
+  def type
+    Employee::ENGINEER
+  end
+
+  def pay_amount
+    monthly_salary
+  end
+end
+
+class Salesman < EmployeeType
+  def type
+    Employee::SALESMAN
+  end
+
+  def pay_amount
+    monthly_salary + commission
+  end
+end
+
+class Manager < EmployeeType
+  def type
+    Employee::MANAGER
+  end
+
+  def pay_amount
+    monthly_salary + bonus
+  end
+end
